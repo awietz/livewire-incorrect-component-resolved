f4cd5f64cd322c3df0ec812b3d4dd73ad5e39413
Alexander Per Wietz <a.wietz@skyfillers.com>
2024-07-24 21:34:51 +0200

    wip

---
+++ app/Livewire/Notifications/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire\Notifications;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function someAction(): void
+    {
+        // Do something
+    }
+
+    public function render()
+    {
+        return view('livewire.notifications.index');
+    }
+}
---
+++ resources/views/livewire/notifications/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <button wire:click="someAction">Some action</button>
+</div>
